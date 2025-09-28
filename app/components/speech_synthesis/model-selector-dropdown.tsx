412ecd0034b46e998a6209cb95505642475a75a2
lbenjak <lora.benjak@gmail.com>
2025-09-28 22:12:20 +0200

    Add model seceltor dropdown component

---
+++ app/components/speech_synthesis/model-selector-dropdown.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './model-selector-dropdown.module.css';
+
+interface ModelSelectorDropdownProps {
+    models: string[];
+    selectedModel: string;
+    handleModelSelect: (model: string) => void;
+}
+
+const ModelSelectorDropdown = ({ models, selectedModel, handleModelSelect }: ModelSelectorDropdownProps) => {
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const newSelectedModel = event.target.value;
+        handleModelSelect(newSelectedModel);
+    };
+
+    return (
+        <div className={styles.modelDropdownContainer}>
+            <label htmlFor="model-select" className={styles.modelLabel}>
+                Model sinteze govora:
+            </label>
+            <select
+                id="model-select"
+                value={selectedModel}
+                onChange={handleChange}
+                className={styles.modelSelect}
+            >
+                <option value="" disabled>
+                    Model sinteze govora:
+                </option>
+                {models.map((model) => (
+                    <option key={model} value={model}>
+                        {model}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default ModelSelectorDropdown;
